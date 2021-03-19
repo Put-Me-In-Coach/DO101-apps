3d42563671916fe6c071c4aa41320d1fb28df24b
Put-Me-In-Coach <alorenzoonline@gmail.com>
2021-03-19 08:16:18 -0400

    version Update

--- version/app.js
+++ version/app.js
@@ -8,8 +8,7 @@ app.get('/', function (req, res) {
 <<<<<<< HEAD
     response = 'This is version 3 of the app.' + '\n';
 =======
-    response = 'This is version 2 of the app.' + '\n';
->>>>>>> 60001e444f12d9a10d1631e65c65eb3818358886
+
 
     //send the response to the client
     res.send(response);
